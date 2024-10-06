98ca8b37241ef0bcceeb62628695e80297db8ea3
khatch2_name <20730681+khatch2@users.noreply.github.com>
2024-10-06 17:50:17 +0200

    Added Book1.xlsx

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         //System.out.println("Hello world!");
         ExcelReader reader = new ExcelReader();
+        String path = "src/Book1.xlsx";
     }
 }
